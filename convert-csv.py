05ba9989574fb0b5f578e259bbb60e5dcc237d31
Claire Donnelly <claire.b.donnelly@gmail.com>
2024-11-01 11:18:56 +0100

    Refactor into a function.

--- convert-csv.py
+++ convert-csv.py
@@ -2,15 +2,28 @@ import pandas as pd
 from pathlib import Path
 from functools import reduce 
 
-path = Path("../streetscapes-data")
-file_paths = path.glob('*.csv')
+def convert_csvs_to_dataframe(files):
+    """
+    Converts a list of global-streetscapes csv files into a single merged dataframe.
 
-dfs = []
-for f in file_paths:
-    df = pd.read_csv(f, dtype={'sequence_id': str})
-    dfs.append(df)
+    Args:
+        files: An iterable of global-streetscapes file paths to csv files.
 
-df_merged = reduce(lambda  left,right: pd.merge(left,right,on=["uuid", "source", "orig_id"],
-                                            how='left'), dfs)
+    Returns:
+        pd.DataFrame resulting from merging all csvs on the
+        columns "uuid", "source", and "orig_id" using a left join.
+    """
+    csv_dfs = []
+    for f in files:
+        df = pd.read_csv(f, dtype={'sequence_id': str})
+        csv_dfs.append(df)
 
-df_merged.to_parquet("../streetscapes-data/streetscapes-data.parquet", compression='gzip')
+    merged_df = reduce(lambda  left,right: pd.merge(left,right,on=["uuid", "source", "orig_id"],
+                                            how='left'), csv_dfs)
+    return merged_df
+
+if __name__ == "__main__":
+    path = Path("../streetscapes-data")
+    file_paths = path.glob('*.csv')
+    streetscapes_df = convert_csvs_to_dataframe(file_paths)
+    streetscapes_df.to_parquet("../streetscapes-data/streetscapes-data.parquet", compression='gzip')
