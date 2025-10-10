ff37d930a1e8caea0f1b34f7b1d67bdeec7bae70
Peter Kalverla <peter.kalverla@gmx.com>
2025-10-10 09:15:59 +0200

    Refine geohash + configure docstring style

--- src/streetscapes/cli/download_images.py
+++ src/streetscapes/cli/download_images.py
@@ -75,27 +75,27 @@ def download_mapillary(
 def _get_geohash_shard_path(geometry):
     """Get nested geo-hash path for given location.
 
-    # Geo-hash precision from
-    # https://python-bloggers.com/2024/02/geohashing-from-scratch-in-python/
-    # Precision          Dimension
-    #         1: 5,000km x 5,000km
-    #         2:   1,250km x 625km
-    #         3:     156km x 156km
-    #         4:   31.9km x 19.5km
-    #         5:   4.89km x 4.89km
-    #         6:   1.22km x 0.61km
-    #         7:       153m x 153m
-    #         8:     38.2m x 19.1m
-    #         9:     4.77m x 4.77m
-    #        10:    1.19m x 0.596m
-    #        11:     149mm x 149mm
-    #        12:   37.2mm x 18.6mm
-    #
-    # Each level of precision subdivides the previous level into 32 subtiles
-    # Shard path of precision 6, split in three parts ab/cd/ef
-    # results in 3 nested folders, each with up to 1024 (32^2) subfolders
-
+    Geo-hash precision from
+    https://python-bloggers.com/2024/02/geohashing-from-scratch-in-python/
+    Precision          Dimension
+            1: 5,000km x 5,000km
+            2:   1,250km x 625km
+            3:     156km x 156km
+            4:   31.9km x 19.5km
+            5:   4.89km x 4.89km
+            6:   1.22km x 0.61km
+            7:       153m x 153m
+            8:     38.2m x 19.1m
+            9:     4.77m x 4.77m
+           10:    1.19m x 0.596m
+           11:     149mm x 149mm
+           12:   37.2mm x 18.6mm
+        Each level of precision subdivides the previous level into 32 subtiles.
+    Shard path of precision 7, split in three parts abc/de/fg
+    abc/ --> region level
+    de/ --> neighbourhood scale (max 32x32 = 1024 per region)
+    fg/ --> block level  (max 32x32 = 1024 per neighbourhood)
     """
     geom = from_wkb(geometry)
-    geohash = pygeohash.encode(geom.y, geom.x, precision=6)  # 1.22 km x 0.61 km
+    geohash = pygeohash.encode(geom.y, geom.x, precision=7)  # 153m x 153m
     return Path(geohash[:2]) / geohash[2:4] / geohash[4:6]
