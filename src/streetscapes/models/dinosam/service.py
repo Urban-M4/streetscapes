244b8331928c6a9d0706edfddfb302425fb43698
cantordust <43831101+cantordust@users.noreply.github.com>
2026-01-16 16:31:44 +0100

    - Added a servable DinoSAM model.

---
+++ src/streetscapes/models/dinosam/service.py
@@ -0,0 +1,68 @@
+import numpy as np
+import orjson as oj
+from pydantic import BaseModel
+
+from streetscapes.models.dinosam.model import DinoSAM
+
+
+class DinoSAMImage(BaseModel):
+    hash: bytes
+    image: bytes
+
+
+class DinoSAMRequest(BaseModel):
+    images: list[DinoSAMImage]
+    prompt: str | list[str]
+
+
+class DinoSAMResponse(BaseModel):
+    hash: bytes
+    labels: list[str]
+    instances: bytes
+
+
+class DinoSAMService:
+    """Inference service for the DinoSAM model.
+
+    Exposes DinoSAM inferece as a structured request/response
+    interface usable by Ray Serve.
+    """
+
+    def __init__(
+        self,
+        sam_model_id: str = "facebook/sam2.1-hiera-large",
+        dino_model_id: str = "IDEA-Research/grounding-dino-base",
+        box_threshold: float = 0.3,
+        text_threshold: float = 0.3,
+        *args,
+        **kwargs,
+    ):
+        self.model = DinoSAM(
+            sam_model_id, dino_model_id, box_threshold, text_threshold, *args, **kwargs
+        )
+
+    def handle(self, request: dict) -> dict:
+        req = DinoSAMRequest(**request)
+
+        hashes = []
+        images = []
+        for entry in req.images:
+            hashes.append(entry.hash)
+            images.append(np.array(oj.loads(entry.image)))
+
+        # Segment the images
+        segmentations = self.model.segment_images(
+            hashes,
+            images,
+            req.prompt,
+        )
+
+        # Construct the response
+        response = []
+        for result in segmentations:
+            result["instances"] = oj.dumps(
+                result["instances"], option=oj.OPT_SERIALIZE_NUMPY
+            )
+            response.append(DinoSAMResponse(**result))
+
+        return response
