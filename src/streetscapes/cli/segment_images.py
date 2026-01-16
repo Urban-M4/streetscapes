25341172298701b08fbfc126a62d69a761d31b0d
cantordust <43831101+cantordust@users.noreply.github.com>
2026-01-16 16:27:36 +0100

    - Updated the CLI definition

--- src/streetscapes/cli/segment_images.py
+++ src/streetscapes/cli/segment_images.py
@@ -1,9 +1,8 @@
-from pathlib import Path
 from cyclopts import App
 
 
 segment_images_cli = App(help="Segment images")
 
-
 segment_images_cli.command("streetscapes.models.bfms.cli:cli", name="bfms")
 segment_images_cli.command("streetscapes.models.maskformer.cli:cli", name="maskformer")
+segment_images_cli.command("streetscapes.models.dinosam.cli:cli", name="dinosam")
