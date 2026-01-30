f4486a1bded9af49a1def61fd7719da288d3d354
cantordust <43831101+cantordust@users.noreply.github.com>
2026-01-30 13:09:54 +0100

    Updates =======
    - DinoSAM model:
        - Switched to image UUIDs instead of raw hashes
        - Added model runs
        - Streamlined the table update step
        - Using the `labels` table
        - Deleted `dinosam/db.py` as it was obsolete.
    
    TODO
    ====
    - Saving segmentations is now handled by the project. The CLI entry points for the other models need to be updated.

--- src/streetscapes/models/dinosam/cli.py
+++ src/streetscapes/models/dinosam/cli.py
@@ -13,7 +13,6 @@ from streetscapes import utils
 from streetscapes import config
 from streetscapes.project import Project
 from streetscapes.serve.server import serve_model
-from streetscapes.models.dinosam.db import save_segmentations
 
 logger = logging.getLogger(__name__)
 
@@ -58,13 +57,6 @@ def cli(
 
     model_name = "dinosam"
 
-    model_params = {
-        "sam_model_id": sam_model_id,
-        "dino_model_id": dino_model_id,
-        "box_threshold": box_threshold,
-        "text_threshold": text_threshold,
-    }
-
     # Open the project
     project = Project(config.get("active_project"))
 
@@ -72,35 +64,52 @@ def cli(
     status = project.get_segmentation_status(collection, model_name, run)
 
     if status is None:
+        logger.info(f"Nothing to segment.")
         return
 
     processed, unprocessed = status
 
     # Initialize Ray Serve handle
     handle = serve_model(model_name, **model_params)
-    logger.info(f"Segmenting {len(unprocessed)} images using DinoSAM...")
+    logger.info(f"Segmenting {len(unprocessed)} images using {model_name}...")
 
     # Rows to be inserted into the database
-    seg_rows = {k: [] for k in Project.core_tables["segmentations"]["schema"]}
-    seg_uuid = project.get_archive_uuid(collection, model_name, run, create=True)
-
-    # Segmentation table update.
-    seg_rows["collection"].append(collection)
-    seg_rows["model"].append(model_name)
-    seg_rows["run"].append(run)
-    seg_rows["archive"].append(ibis.uuid(seg_uuid).to_pyarrow())
-    seg_rows["params"].append(oj.dumps(model_params))
+    seg_data = {k: [] for k in Project.core_tables["segmentations"]["schema"]}
+    run_uid = project.get_segmentation_run_uid(collection, model_name, run, create=True)
 
     # Update the segmentation database
-    project._con.insert("segmentations", seg_rows)
+    project._con.insert("segmentations", seg_data)
     archive_path = utils.ensure_dir(
-        project.get_archive_path(model_name, create=True) / str(seg_uuid)
+        project.get_archive_path(model_name, create=True) / str(run_uid)
     )
+
+    label_data = {
+        k: [None for _ in range(len(unprocessed))]
+        for k in project.core_tables["labels"]["schema"]
+    }
+
+    # Add the segmentation run to the databse
+    model_params = {
+        "sam_model_id": sam_model_id,
+        "dino_model_id": dino_model_id,
+        "box_threshold": box_threshold,
+        "text_threshold": text_threshold,
+    }
+    seg_data = {
+        "collection": collection,
+        "model": model_name,
+        "run": run,
+        "archive": ibis.uuid(run_uid).to_pyarrow(),
+        "params": oj.dumps(model_params),
+    }
+    project.update_table("segmentations", seg_data)
+
+    logger.info(f"Segmenting {len(unprocessed)} images...")
 
     for batch in batched(unprocessed.items(), batch_size):
 
         # Extract the hashes
-        hashes = [b[0] for b in batch]
+        uids = [b[0] for b in batch]
         images = [np.asarray(iio.imread(b[1])) for b in batch]
 
         # Process the images
@@ -110,7 +119,7 @@ def cli(
                     "hash": h,
                     "image": oj.dumps(image, option=oj.OPT_SERIALIZE_NUMPY),
                 }
-                for h, image in zip(hashes, images)
+                for h, image in zip(uids, images)
             ],
             "prompt": prompt,
         }
@@ -118,7 +127,22 @@ def cli(
 
         logger.info(f"Successfully segmented {len(images)} images, saving instances.")
 
-        # Store the segmentations and their metadata
-        save_segmentations(
-            project, model_name, run, model_params, responses, archive_path
-        )
+        for idx, response in enumerate(responses):
+
+            label_data["image"][idx] = ibis.uuid(response.uuid).to_pyarrow()
+            label_data["model"][idx] = model_name
+            label_data["run"][idx] = run
+            label_data["labels"][idx] = response.labels
+
+            # The file name is constructed from the UUID.
+            seg_fname = f"{response.uuid}.npz"
+
+            # Save the segmentations.
+            logger.info(f"[ Saving segmentation '{seg_fname}'...")
+            instances = oj.loads(response.instances)
+
+            project.save_segmentation(archive_path, instances)
+
+
+    # Add the labels to the database
+    project.update_table('labels', label_data)
