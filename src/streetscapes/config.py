b4ddfcbfd567113d62f7105d3c90c56dce855f6e
cantordust <43831101+cantordust@users.noreply.github.com>
2026-03-02 14:16:30 +0100

    - Moved configuration to Pydantic Settings.

--- src/streetscapes/config.py
+++ src/streetscapes/config.py
@@ -1,48 +1,23 @@
 import os
-from omegaconf import OmegaConf
+from pydantic import field_validator
+from pydantic_settings import BaseSettings
+from pathlib import Path
+from streetscapes.utils import ensure_dir
 from platformdirs import user_config_path, user_cache_path, user_data_path
 
 CONFIG_FILE = user_config_path("streetscapes", ensure_exists=True) / "config.json"
 
 
-DEFAULTS = {
-    "project_dir": user_data_path("streetscapes"),
-    "image_dir": user_cache_path("streetscapes"),
-    "active_project": "streetscapes",
-    "mapillary_token": os.getenv("MAPILLARY_TOKEN", ""),
-}
+class Configuration(BaseSettings):
+    project_dir: Path = user_data_path("streetscapes")
+    image_dir: Path = user_cache_path("streetscapes")
+    active_project: str = "streetscapes"
+    mapillary_token: str = os.getenv("MAPILLARY_TOKEN", "")
+
+    @field_validator("project_dir", "image_dir", mode="before")
+    @classmethod
+    def _ensure_dir(cls, value: str | Path) -> Path:
+        return ensure_dir(value)
 
 
-def initialize_config():
-    """Create config file with defaults if it doesn’t exist."""
-    if not CONFIG_FILE.exists():
-        cfg = OmegaConf.create(DEFAULTS)
-        OmegaConf.save(cfg, CONFIG_FILE)
-
-
-def load() -> dict:
-    """Load config, initializing it if necessary."""
-    initialize_config()
-    cfg = OmegaConf.load(CONFIG_FILE)
-    # Optional: fill in missing keys for forward compatibility
-    new_keys = set(DEFAULTS.keys()).difference(set(cfg.keys()))
-    for k in new_keys:
-        cfg.setdefault(k, DEFAULTS[k])
-
-    # Optional: remove deprecated keys that are not in DEFAULTS
-    deprecated_keys = set(cfg.keys()).difference(set(DEFAULTS.keys()))
-    for k in deprecated_keys:
-        cfg.pop(k)
-
-    return cfg
-
-
-def getopt(key: str, default=None):
-    return load().get(key, default)
-
-
-def setopt(key: str, value):
-    cfg = load()
-    if key in DEFAULTS:
-        cfg[key] = value
-        OmegaConf.save(cfg, CONFIG_FILE)
+conf = Configuration(CONFIG_FILE)
