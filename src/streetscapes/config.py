38ae0965d00b84b733a9599a4cf57382dfc90066
cantordust <43831101+cantordust@users.noreply.github.com>
2026-03-02 14:40:28 +0100

    - Updated the configuration class.

--- src/streetscapes/config.py
+++ src/streetscapes/config.py
@@ -5,19 +5,33 @@ from pathlib import Path
 from streetscapes.utils import ensure_dir
 from platformdirs import user_config_path, user_cache_path, user_data_path
 
-CONFIG_FILE = user_config_path("streetscapes", ensure_exists=True) / "config.json"
-
 
 class Configuration(BaseSettings):
+    config_file: Path = user_config_path("streetscapes") / "config.json"
     project_dir: Path = user_data_path("streetscapes")
     image_dir: Path = user_cache_path("streetscapes")
     active_project: str = "streetscapes"
     mapillary_token: str = os.getenv("MAPILLARY_TOKEN", "")
 
-    @field_validator("project_dir", "image_dir", mode="before")
+    @field_validator("project_dir", mode="before")
     @classmethod
-    def _ensure_dir(cls, value: str | Path) -> Path:
+    def _ensure_project_dir(cls, value: str | Path) -> Path:
         return ensure_dir(value)
 
+    @field_validator("image_dir", mode="before")
+    @classmethod
+    def _ensure_image_dir(cls, value: str | Path) -> Path:
+        return ensure_dir(value)
 
-conf = Configuration(CONFIG_FILE)
+    @field_validator("config_file", mode="before")
+    @classmethod
+    def _ensure_config_file(cls, value: str | Path) -> Path:
+        value = Path(value)
+        Path.touch(value)
+        return value
+
+    def save(self):
+        self.config_file.write_text(self.model_dump_json(indent=4))
+
+
+conf = Configuration()
