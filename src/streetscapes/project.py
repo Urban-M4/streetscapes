51299c66ef16c855c8a63a6f8d1da651ee5fa8c0
cantordust <43831101+cantordust@users.noreply.github.com>
2026-04-07 14:22:26 +0200

    - Fix for missing constraints on table creation.

--- src/streetscapes/project.py
+++ src/streetscapes/project.py
@@ -83,7 +83,7 @@ class Project:
                 "polygons": "GEOMETRY",
             },
             "init": [
-                "ALTER TABLE segmentations ADD PRIMARY KEY (run, curated, image);"
+                "ALTER TABLE segmentations ADD PRIMARY KEY (run, curated, image)"
             ],
         },
         "mapillary": {
@@ -221,7 +221,7 @@ class Project:
         name: str,
         schema: dict | ibis.Schema | None = None,
         overwrite: bool = False,
-    ) -> ibis.Table:
+    ) -> None:
         """Ensure that a table exists with the given schema.
 
         Args:
@@ -250,7 +250,15 @@ class Project:
         )
         sql = f"{sql} ({fields})"
 
-        return self._con.raw_sql(sql)
+        self._con.raw_sql(sql)
+
+        # Run init clauses, if any
+        inits = table.get("init", [])
+        print(f"==[ {name=} | inits: {inits}")
+        for sql in inits:
+            self._con.raw_sql(sql)
+
+
 
     def get_image_dir_for_source(
         self,
@@ -294,7 +302,12 @@ class Project:
         uids = [uuid.UUID(u) if isinstance(u, str) else u for u in uids]
 
         t = self.table("images")
-        results = t.filter([t.uuid.isin(uids)]).select(["uuid", "source", "shard"]).to_pyarrow().to_pylist()
+        results = (
+            t.filter([t.uuid.isin(uids)])
+            .select(["uuid", "source", "shard"])
+            .to_pyarrow()
+            .to_pylist()
+        )
 
         paths: dict[uuid.UUID, tuple[Path, str]] = {}
         for result in results:
@@ -449,7 +462,7 @@ class Project:
         t = self.table("segmentations")
         if t is None:
             raise ValueError
-    
+
         t = t.filter([t.run == run, t.curated == curated, t.image == image])
 
         result = t.to_pyarrow().to_pylist()
@@ -579,7 +592,6 @@ class Project:
         missing = list(set(uids).difference(processed))
         unprocessed = self.get_image_paths_from_uuids(missing)
         return processed, unprocessed
-
 
     def add_images(
         self,
