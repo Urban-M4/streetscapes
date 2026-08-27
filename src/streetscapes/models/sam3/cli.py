847b84a3a19e5d3db3ce5f1d571df1ee978b5201
cantordust <43831101+cantordust@users.noreply.github.com>
2026-08-27 11:35:34 +0200

    - Added support for image segmentation with SAM3.

---
+++ src/streetscapes/models/sam3/cli.py
@@ -0,0 +1,123 @@
+"""Command line interface for BFMS model."""
+
+import logging
+from itertools import batched
+from typing import cast
+
+import imageio.v3 as iio
+import numpy as np
+import orjson as oj
+
+from streetscapes import CFG, utils
+from streetscapes.project import Project
+from streetscapes.serve.server import serve_model
+from streetscapes.utils.masks import mask2poly
+
+logger = logging.getLogger(__name__)
+
+
+def cli(
+    prompt: str,
+    weights: str = 'sam3.pt',
+    image_path: str | None = None,
+    batch_size: int = 10,
+    confidence: float = 0.25,
+    quantisation: str | None = None,
+    run: str | None = None,
+    project: str = cast("str", CFG.active_project),
+    overwrite: bool = False,
+    verbose: bool = False,
+):
+    """Segment images with DinoSAM.
+
+    Args:
+        prompt: The prompt to use for this model.
+        weights: Path to the SAM3 model weights. Defaults to 'sam3.pt' in the current directory.
+            This can be a symlink to the actual weights located elsewhere.
+        image_path: Path to an image or a directory of images.
+            If not provided uses all downloaded images in the project.
+        batch_size: Batch size for the segmenter.
+        device: Specify a device to run the model on.
+        confidence: Confidence threshold for accepting segmentations.
+        quantisation: Quantisation level. Possible values are `FP16` (faster inference) or `FP32`.
+            `None` means that the default value will be used.
+        overwrite: Whether to overwrite existing segmentations.
+        run: Model run ID.
+        project: The project to use.
+        overwrite: Overwrite an existing run.
+        verbose: Print verbose log to the terminal. Useful for debugging models.
+    """
+
+    # Open the project
+    proj = Project(project)
+
+    # Save the run metadata.
+    # ==================================================
+    model = "sam3"
+    model_params = {
+        "weights": weights,
+        "confidence": confidence,
+        "quantisation": quantisation,
+    }
+
+    result = proj.add_run(run, model, model_params, overwrite)
+    run = str(result.get("run")[0])  # type: ignore[index]
+
+    # Get all images that need to be processed.
+    # ==================================================
+    if image_path is not None:
+        image_paths = utils.get_image_paths(image_path)
+        if len(image_paths) == 0:
+            logger.info(f"Nothing to process.")
+            return
+
+        uids = list(map(utils.get_image_uuid, image_paths))
+    else:
+        uids = proj.get_image_uuids()
+    _, unprocessed = proj.get_segmentation_status(uids, run)
+
+    if len(unprocessed) == 0:
+        logger.info(f"Nothing to process.")
+        return
+
+    handle = serve_model(model, verbose, **model_params)
+    logger.info(f"Segmenting {len(unprocessed)} images using {model}...")
+    batches = list(batched(unprocessed, batch_size))
+    for batch_idx, batch in enumerate(batches, 1):
+
+        # Extract the paths and open the images as NumPy arrays.
+        request = {
+            "images": [],
+            "prompt": prompt,
+        }
+        for uid in batch:
+            path, _ = unprocessed[uid]
+            img_data = {
+                "uid": uid,
+                "image": oj.dumps(
+                    np.asarray(iio.imread(path)), option=oj.OPT_SERIALIZE_NUMPY
+                ),
+            }
+            request["images"].append(img_data)  # type: ignore[attr-defined]
+
+        # Process the images.
+        logger.info(f"Segmenting batch [{batch_idx:>4d}/{len(batches):>4d}]...")
+        responses = handle.remote(request).result()
+        logger.debug(f"Successfully segmented {len(batch)} images, saving instances.")
+
+        # Save the instances.
+        segmentations = []
+        for response in responses:
+            instances = oj.loads(response.instances)
+            instances = np.array(instances) # turned 3-level nested list into 3D array
+            segmentations.append(
+                {
+                    "run": run,
+                    "image": response.uid,
+                    "labels": response.labels,
+                    "polygons": mask2poly(instances, model="dinosam"),
+                }
+            )
+
+        # Update the segmentation table.
+        proj.add_segmentations(segmentations, overwrite)
---
+++ src/streetscapes/models/sam3/model.py
@@ -0,0 +1,122 @@
+import numpy as np
+import uuid
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+from streetscapes import utils
+from streetscapes.utils import logger
+
+
+class SAM3:
+
+    def __init__(
+        self,
+        weights: str | Path = "sam3.pt",
+        device: str | None = None,
+        confidence: float = 0.25,
+        quantisation: str | None = None,
+        *args,
+        **kwargs,
+    ):
+        """A SAM3 backend for StreetScapes.
+        Uses the Ultralytics engine to allow for querying for multiple types of objects.
+
+        IMPORTANT: The model weights need to be downloaded *manually*.
+        To do that, you need to sign up to the SAM3 HuggingFace repository here:
+        https://huggingface.co/facebook/sam3
+
+        Args:
+            weights: Path to the SAM3 model weights. Defaults to 'sam3.pt' in the current directory.
+                This can be a symlink to the actual weights located elsewhere.
+            device: Specify a device to run the model on.
+            confidence: Confidence threshold for accepting segmentations.
+            quantisation: Quantisation level. Possible values are `FP16` (faster inference) or `FP32`.
+                `None` means that the default value will be used.
+        """
+
+        from ultralytics.models.sam import SAM3SemanticPredictor
+
+        self.device = utils.get_device(device)
+
+        # Model parameters
+        # ==================================================
+        self.confidence = confidence
+        self.quantisation = quantisation
+
+        # SAM3 model
+        # ==================================================
+        # Overrides
+
+        weight_path = Path(weights)
+
+        if not weight_path.is_absolute():
+            weight_path = Path(__file__).parent.resolve() / weight_path
+
+        overrides = {
+            "model": str(weight_path),
+            "task": "segment",
+            "mode": "predict",
+            "save": False,
+            "conf": confidence,
+        }
+
+        if isinstance(quantisation, str):
+            overrides["quantize"] = quantisation.lower()
+
+        self.model = SAM3SemanticPredictor(overrides=overrides)
+
+    def segment_images(
+        self,
+        uids: list[uuid.UUID],
+        images: list[np.ndarray],
+        prompt: str | list[str],
+    ) -> list[dict]:
+        """Segment the provided sequence of images.
+
+        Args:
+            uids: UUID values associated with the images.
+                This is used for keeping track of which images have been segmented,
+                regardless of the file name and where they are stored.
+            images: A list (batch) of images as NumPy arrays.
+            prompt: A prompt as a string or a list of strings,
+                indicating the categories (labels).
+
+        Returns:
+            A list of dictionaries containing instance-level segmentation information.
+        """
+
+        # Flatten the label dictionary
+        _prompt = utils.extract_categories(prompt, as_list=True)
+
+        segmentations = []
+
+        for idx, (uid, image) in tqdm(enumerate(zip(uids, images)), total=len(images)):
+            # Dictionary that will hold all the information about the segmentation
+            segmentation = {"uid": uid}
+
+            self.model.set_image(Image.fromarray(image.astype(np.uint8)).convert("RGB"))
+
+            # Segment the objects with SAM3
+            # ==================================================
+            result = self.model(text=_prompt)[0]
+
+            # Process the model outputs
+            masks = result.masks.data.cpu().numpy().squeeze()
+
+            # Instance labels extracted from the class IDs
+            instance_labels = [result.names[int(c)] for c in result.boxes.cls]
+
+            # Populate the instance masks.
+            instances = np.zeros(
+                (len(instance_labels), *image.shape[:2]),
+                dtype=np.bool_,
+            )
+            for mask_idx, sam_mask in enumerate(masks):
+                instances[mask_idx][sam_mask > 0] = True
+
+            # Extract and store the segmentations.
+            segmentation["labels"] = instance_labels
+            segmentation["instances"] = instances
+            segmentations.append(segmentation)
+
+        return segmentations
