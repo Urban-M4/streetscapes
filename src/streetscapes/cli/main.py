c633627ddbff02e197bf3fc8547211d75ea0f3c3
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2026-09-18 14:46:26 +0200

    Remove unimplemented cli stubs

--- src/streetscapes/cli/main.py
+++ src/streetscapes/cli/main.py
@@ -15,9 +15,6 @@ from streetscapes.cli.fetch_metadata import fetch_metadata_cli
 from streetscapes.cli.images import image_cli
 from streetscapes.cli.segment_images import segment_images_cli
 
-# from streetscapes.cli.finetune_model import finetune_model_cli
-# from streetscapes.cli.identify_buildings import identify_buildings_cli
-
 load_dotenv()
 
 app = App(help="Street view image analysis toolkit")
@@ -31,8 +28,6 @@ app.command(export_cli, name="export")
 app.command(fetch_metadata_cli, name="fetch-metadata")
 app.command(download_images_cli, name="download-images")
 app.command(segment_images_cli, name="segment-images")
-# app.add(finetune_model_cli, name="finetune_model")
-# app.add(identify_buildings_cli, name="identify_buildings")
 
 if __name__ == "__main__":
     app()
